16fa186b0a59a145ffd986833237cbdb131a7ca3
Lucas Mouilleron <lucas.mouilleron@me.com>
2015-11-22 13:16:56 +0100

    config test

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<h1>Hello dockerManager !</h1>
-
-Running revision <?php echo getenv("REVISION") ?>.
-
 <?php
 $ENVIRONMENT = "local";
 if (getenv("ENVIRONMENT") !== false) $ENVIRONMENT = getenv("ENVIRONMENT");
 include __DIR__ . "/" . $ENVIRONMENT . ".config.php";
 ?>
 
-This is a config test : <?php echo TEST; ?>
+<h1>Hello dockerManager !</h1>
+
+<h2>Running revision <?php echo getenv("REVISION") ?>.</h2>
+<h2>This is a config test : <?php echo TEST; ?></h2>
