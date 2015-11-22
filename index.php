c5da799d8b8b7ee994f1fa58c7a8432f1fe095f3
Lucas Mouilleron <lucas.mouilleron@me.com>
2015-11-22 18:01:48 +0100

    mysql conf

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $ENVIRONMENT = "local";
-if (getenv("ENVIRONMENT") !== false) $ENVIRONMENT = getenv("ENVIRONMENT");
+if (getenv("ENVIRONMENT") !== false && in_array(getenv("ENVIRONMENT"), array("local", "preprod"))) $ENVIRONMENT = getenv("ENVIRONMENT");
 include __DIR__ . "/" . $ENVIRONMENT . ".config.php";
 ?>
 
