7a482f945bed667e15ba1353d77ad54987a0b708
Lucas Mouilleron <lucas.mouilleron@me.com>
2015-11-22 17:48:09 +0100

    sql

--- index.php
+++ index.php
@@ -8,3 +8,24 @@ include __DIR__ . "/" . $ENVIRONMENT . ".config.php";
 
 <h2>Running revision <?php echo getenv("REVISION") ?>.</h2>
 <h2>This is a config test : <?php echo TEST; ?></h2>
+
+<?php
+$servername = "localhost";
+$username = "mysql";
+$password = "thepass";
+$dbname = "main";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+
+$sql = "SELECT * FROM test";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        print_r($row);
+    }
+} else  echo "0 results";
+$conn->close();
+
+?>
