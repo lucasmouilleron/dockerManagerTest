f069292637ad0a5909f3bdd89c49d15bdd343074
Lucas Mouilleron <lucas.mouilleron@me.com>
2015-11-22 17:58:04 +0100

    mysql conf

--- index.php
+++ index.php
@@ -9,18 +9,11 @@ include __DIR__ . "/" . $ENVIRONMENT . ".config.php";
 <h2>Running revision <?php echo getenv("REVISION") ?>.</h2>
 <h2>This is a config test : <?php echo TEST; ?></h2>
 
+<h2>This is database data : </h2>
 <?php
-echo "herel";
-$servername = "127.0.0.1";
-$username = "mysql";
-$password = "thepass";
-$dbname = "main";
-echo "here1";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-echo "here20";
+$conn = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
-echo "connected";
 $sql = "SELECT * FROM test";
 $result = $conn->query($sql);
 
@@ -30,5 +23,4 @@ if ($result->num_rows > 0) {
     }
 } else  echo "0 results";
 $conn->close();
-echo "here2";
 ?>
--- preprod.config.php
+++ preprod.config.php
@@ -1,3 +1,8 @@
 <?php
 
+define("MYSQL_HOST","127.0.0.1");
+define("MYSQL_USER",getenv("MYSQL_USER"));
+define("MYSQL_PASSWORD",getenv("MYSQL_PASS"));
+define("MYSQL_DATABASE",getenv("MYSQL_DATABASE"));
+
 define("TEST","from preprod config");
